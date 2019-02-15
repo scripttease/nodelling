021b327d0e919739fc7af60b7968d52c8b143ab4
Alice Dee <alice.dee@guardian.co.uk>
2019-02-15 17:37:57 +0000

    adds data handling to extract streak info

--- data-handling.js
+++ data-handling.js
@@ -1,9 +1,107 @@
 function dataHandling(json) {
-  var txt = JSON.parse(json)
-  var stng = txt.toString()
-
-
-  return stng.split("\n");
+  const txt = JSON.parse(json)
+  const stng = txt.toString()
+  const lines = stng.split("\n")
+  // first capture group is commits second is date
+  return lines
 }
 
-module.exports = dataHandling;
+function extractData(lines) {
+
+  const regex = /data-count="([0-9]+)" data-date="(20[0-9][0-9]-[0-9][0-9]-[0-9][0-9])/
+  const map1 = lines.map(line => regex.exec(line));
+  const filt = map1.filter(x => x != null);
+  // fat arrow syntax is more complicated with objects (needd a ( before {
+  const kvMapObjArr = filt.map(function(x) {
+    return {
+      commits: parseInt(x[1]),
+      date: Date.parse(x[2],)
+    };
+  });
+  return kvMapObjArr;
+}
+
+function countStreak(kvMapObjArr) {
+  // console.log(kvMapObjArr);
+  // array
+  // for each val in kvMapObjArr if commits <0 streaklength += 1
+  // elseif (commits are 0) and if streaklength <1 push object into array containing val date, val streak length and calc val of start date (date - streak length in days) and set streaklength == 0 again
+  let streakObj;
+  const streakArray = [];
+  let streakLength = 0;
+  kvMapObjArr.forEach(function(obj) {
+    // console.log(obj.commits);
+    if (obj.commits > 0) {
+      streakLength += 1
+      // console.log(streakLength);
+    } else if (obj.commits < 1 && streakLength > 0) {
+      streakObj = {
+        streak: streakLength,
+        endDate: obj.date,
+        startDate: obj.date - (streakLength * 24 * 3600000) ,
+      }
+      streakArray.push(streakObj)
+      streakLength = 0
+
+    } else {
+      streakLength = 0
+    }
+    //nb forEach doesn't return it is side-effecting only
+  });
+  return streakArray;
+}
+
+function longestStreak(streakArray) {
+  // var longestStreak;
+  console.log(streakArray);
+  const longestStreak = streakArray.reduce(function(acc, curr) {
+    return (curr.streak >= acc.streak) ? curr : acc
+  });
+  console.log(longestStreak);
+  return longestStreak
+}
+
+
+
+
+
+
+
+
+
+function louisCountStreak(days) {
+  const streaks = [];
+
+  while (days.length > 0) {
+    const streak = collectNextStreak(days);
+    if (streak.length > 0) {
+      streaks.push(streak);
+    }
+  }
+
+  return streaks;
+}
+
+function collectNextStreak(days) {
+  const streakDays = [];
+
+  discardNoCommitDays(days);
+
+  while (days[0] && days[0].commits > 0) {
+    streakDays.push(days.shift());
+  }
+
+  return streakDays;
+}
+
+function discardNoCommitDays(days) {
+    while (days[0] && days[0].commits == 0) {
+    days.shift();
+  }
+}
+
+module.exports.dataHandling = dataHandling;
+module.exports.extractData = extractData;
+module.exports.countStreak = countStreak;
+module.exports.longestStreak = longestStreak;
+module.exports.louisCountStreak = louisCountStreak;
--- test/test.js
+++ test/test.js
@@ -1,41 +1,86 @@
-var fs  = require('fs');
-// var fileName = './user-test-data.txt'
-var chai = require('chai');
-var server = require('../server');
-var dataHandling = require('../data-handling');
-// var testData = require('./user-test-data.txt');
-var should = chai.should();
+const fs  = require('fs');
+// const fileName = './user-test-data.txt'
+const chai = require('chai');
+const server = require('../server');
+const dataHandling = require('../data-handling').dataHandling;
+const extractData = require('../data-handling').extractData;
+const countStreak = require('../data-handling').countStreak;
+const longestStreak = require('../data-handling').longestStreak;
+// const testData = require('./user-test-data.txt');
+const should = chai.should();
 
 // TODO how could i use const here when I want the data available elsewhere in scope?
-// before(function(done){
 
 describe("dataHandling", function() {
 
-  before(function(done){
+  before(function(done) {
     // Reads test file and converts to JSON so that it will have same format as the data I request
     //TODO make this relative
     fs.readFile('/Users/al/projects/nodelling/test/user-test-data.txt', 'utf8', function(err, fileContents,) {
       if (err) throw(err);
 
-      var data = JSON.stringify(fileContents);
-      // console.log(fileContents);
-      done()
-      return data;
+      done();
+      return data = JSON.stringify(fileContents);
     })
-    return data;
+    // return data;
   });
 
-  it("should convert json to string", function() {
-    var stringOut = dataHandling(data);
-
-    // console.log(data);
-    expect(stringOut.charAt(1)).to.equal('<')
-  });
 
   it('should split data into array of lines', function() {
-    var stringOut = dataHandling(data);
-    console.log(stringOut);
-    expect(stringOut.length).to.equal(577);
+    const stringOut = dataHandling(data);
+
+    // expect(stringOut.length).to.equal(577)
+    expect(stringOut.length).to.equal(577)
+  });
+
+  describe('extractData', function() {
+    it('should return array of objects', () => {
+
+      const stringOut = dataHandling(data);
+      // console.log(stringOut[9]);
+      const objOut = extractData(stringOut);
+      // console.log(objOut[3]);
+      const expectedOut = {
+        commits: 0,
+        date: Date.parse('2018-02-14'),
+        // date: '2018-02-14',
+      }
+      console.log(expectedOut);
+      expect(objOut[3]).to.deep.equal(expectedOut)
+
+    });
+  })
+
+  describe('#countStreak', () => {
+    it('should creat array of streak objects with date', () => {
+
+      const stringOut = dataHandling(data);
+      const objOut = extractData(stringOut);
+      const streakArr = countStreak(objOut);
+      expect(streakArr.length).to.equal(24);
+
+    });
+  });
+
+  describe('#longestStreak', function() {
+    it(' should count consecutive streaks and return longest', () => {
+
+      const stringOut = dataHandling(data);
+      const objOut = extractData(stringOut);
+      const streakArr = countStreak(objOut);
+      // console.log(streakArr);
+      const longest = longestStreak(streakArr)
+      expect(longest.streak).to.equal(4);
+
+
+    });
   });
 
 });
+
+// it("should convert json to string", function() {
+//   const stringOut = dataHandling(data);
+
+//   // console.log(data);
+//   expect(stringOut.charAt(1)).to.equal('<')
+// });
